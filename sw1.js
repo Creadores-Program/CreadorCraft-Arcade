9724b3994083256843d16fbdfa1d93f5214d0f8f
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-03-09 12:57:59 -0600

    Update and rename sw.js to sw1.js

--- sw1.js
+++ sw1.js
@@ -2,8 +2,5 @@ self.addEventListener("install", function(event) {
    console.log("Service worker installed");});
 self.addEventListener("activate", function(event) {
    console.log("Service worker activated");});
-self.addEventListener('fetch', function(event) {});
-self.addEventListener('message', function(event) {});
-self.addEventListener('push', function(event) {});
 
 
