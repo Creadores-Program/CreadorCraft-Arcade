985252bcb8c03dcc000daeb62180f508f147093e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-03-08 18:31:26 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// This code executes in its own worker or thread
-self.addEventListener("install", event => {
+
+kkself.addEventListener("install", function(event) {
    console.log("Service worker installed");
-   });
-self.addEventListener("activate", event => {
+});
+self.addEventListener("activate", function(event) {
    console.log("Service worker activated");
 });
