601b028fbd8ff1137a00f85aad9cc2b663009b07
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-03-08 19:31:24 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 self.addEventListener("install", function(event) {
-   console.log("Service worker installed");
-});
+   console.log("Service worker installed");});
 self.addEventListener("activate", function(event) {
-   console.log("Service worker activated");
-});
+   console.log("Service worker activated");});
+self.addEventListener('fetch', function(event) {});
+self.addEventListener('message', function(event) {});
+self.addEventListener('push', function(event) {});
+
 
