066e1c32118b75723104d3ff527067c4cca57441
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-01-02 11:26:37 -0600

    Crear CommonJS para Módulos

---
+++ Games/CommonJSModule.js
@@ -0,0 +1,12 @@
+(function(root){
+  let modules = {};
+  async function requireModule (path){
+    if(Object.keys(modules).indexOf(path) < 0){
+      let blob = await GameProps.getFileGame().file(path).async("blob");
+      modules[path] = URL.createObjectURL(blob);
+    }
+    const module = await import(modules[path]);
+    return module;
+  }
+  root.requireModule = requireModule;
+})(typeof window !== 'undefined') ? window : global);
