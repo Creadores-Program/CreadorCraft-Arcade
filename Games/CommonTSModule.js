4b1166cf214941c4b038da3c0075b96764ac2508
Trollhunters501PC <cpcreador@gmail.com>
2025-01-02 20:05:21 -0600

    Añadir CommonTS tipo modulo

---
+++ Games/CommonTSModule.js
@@ -0,0 +1,13 @@
+(function(root){
+  let modules = {};
+  async function requireTSModule (path){
+    if(Object.keys(modules).indexOf(path) < 0){
+      let str = await GameProps.getFileGame().file(path).async("string");
+      let blob = new Blob([window.ts.transpile(str, { target: window.ts.ModuleKind.ES2015 })], { type: 'text/typescript' });
+      modules[path] = URL.createObjectURL(blob);
+    }
+    const module = await import(modules[path]);
+    return module;
+  }
+  root.requireTSModule = requireTSModule;
+})((typeof window !== 'undefined') ? window : global);
