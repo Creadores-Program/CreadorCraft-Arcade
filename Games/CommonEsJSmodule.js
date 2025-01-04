90cf3dc63047af2fb3c25d52893fc41273eb34a8
Trollhunters501PC <cpcreador@gmail.com>
2025-01-03 19:10:09 -0600

    añadir EsJS Common para Modulos

---
+++ Games/CommonEsJSmodule.js
@@ -0,0 +1,14 @@
+import * as EsJS from 'https://esm.run/@es-js/core';
+(function(root){
+  let modules = {};
+  async function requireEsJSModule (path){
+    if(Object.keys(modules).indexOf(path) < 0){
+      let str = await GameProps.getFileGame().file(path).async("string");
+      let blob = new Blob([EsJS.compile(str)], { type: 'text/javascript' });
+      modules[path] = URL.createObjectURL(blob);
+    }
+    const module = await import(modules[path]);
+    return module;
+  }
+  root.requireTSModule = requireEsJSModule;
+})((typeof window !== 'undefined') ? window : global);
