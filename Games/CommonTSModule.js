389bccf477e6156102e6c8001ad160c658c7f4d9
Trollhunters501PC <cpcreador@gmail.com>
2025-01-03 19:10:47 -0600

    Arreglar CommonTsModule

--- Games/CommonTSModule.js
+++ Games/CommonTSModule.js
@@ -3,7 +3,7 @@
   async function requireTSModule (path){
     if(Object.keys(modules).indexOf(path) < 0){
       let str = await GameProps.getFileGame().file(path).async("string");
-      let blob = new Blob([window.ts.transpile(str, { target: window.ts.ModuleKind.ES2015 })], { type: 'text/typescript' });
+      let blob = new Blob([window.ts.transpile(str, { target: window.ts.ModuleKind.ES2015 })], { type: 'text/javascript' });
       modules[path] = URL.createObjectURL(blob);
     }
     const module = await import(modules[path]);
