3d4ab5ccfae7f63c163e0331ec1e4a9964895f24
Trollhunters501PC <cpcreador@gmail.com>
2025-03-01 19:02:27 -0600

    Arreglar error de MimeType que JSzip no define....

--- Games/CommonJSModule.js
+++ Games/CommonJSModule.js
@@ -1,12 +1,24 @@
 (function(root){
   let modules = {};
-  async function requireModule (path){
-    if(Object.keys(modules).indexOf(path) < 0){
-      let blob = await GameProps.getFileGame().file(path).async("blob");
-      modules[path] = URL.createObjectURL(blob);
+
+  function getMimeType(path) {
+    if (path.endsWith(".js")) return "application/javascript";
+    if (path.endsWith(".json")) return "application/json";
+    if (path.endsWith(".css")) return "text/css";
+    return "application/octet-stream";
+  }
+
+  async function requireModule(path) {
+    if (!modules.hasOwnProperty(path)) {
+      let originalBlob = await GameProps.getFileGame().file(path).async("blob");
+      let mime = getMimeType(path);
+      // Crea un nuevo Blob con el MIME type definido
+      let fixedBlob = new Blob([originalBlob], { type: mime });
+      modules[path] = URL.createObjectURL(fixedBlob);
     }
     const module = await import(modules[path]);
     return module;
   }
+
   root.requireModule = requireModule;
 })((typeof window !== 'undefined') ? window : global);
