9bf7da7707b67f605c4a0c91e029c021d234e34d
Trollhunters501PC <cpcreador@gmail.com>
2025-03-01 19:21:42 -0600

    Update CommonJSModule.js

--- Games/CommonJSModule.js
+++ Games/CommonJSModule.js
@@ -1,23 +1,12 @@
 (function(root){
   let modules = {};
-
-  function getMimeType(path) {
-    if (path.endsWith(".js")) return "application/javascript";
-    if (path.endsWith(".json")) return "application/json";
-    if (path.endsWith(".css")) return "text/css";
-    return "application/octet-stream";
-  }
-
-  async function requireModule(path) {
-    if (!modules.hasOwnProperty(path)) {
-      let originalBlob = await GameProps.getFileGame().file(path).async("string");
-      let mime = getMimeType(path);
-      let fixedBlob = new Blob([originalBlob], { type: mime });
-      modules[path] = URL.createObjectURL(fixedBlob);
+  async function requireModule (path){
+    if(Object.keys(modules).indexOf(path) < 0){
+      let blob = await GameProps.getFileGame().file(path).async("blob");
+      modules[path] = URL.createObjectURL(blob);
     }
     const module = await import(modules[path]);
     return module;
   }
-
   root.requireModule = requireModule;
 })((typeof window !== 'undefined') ? window : global);
