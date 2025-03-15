3411963626696b373d9ab6df1b7f957da4846af7
Trollhunters501PC <cpcreador@gmail.com>
2025-03-15 13:12:52 -0600

    Arreglar Blob

--- Games/CommonJSModule.js
+++ Games/CommonJSModule.js
@@ -1,12 +1,23 @@
 (function(root){
   let modules = {};
-  async function requireModule (path){
-    if(Object.keys(modules).indexOf(path) < 0){
-      let blob = await GameProps.getFileGame().file(path).async("blob");
-      modules[path] = URL.createObjectURL(blob);
+
+  function getMimeType(path) {
+    if (path.endsWith(".js")) return "application/javascript";
+    if (path.endsWith(".json")) return "application/json";
+    if (path.endsWith(".css")) return "text/css";
+    return "application/octet-stream";
+  }
+
+  async function requireModule(path) {
+    if (!modules.hasOwnProperty(path)) {
+      let originalBlob = await GameProps.getFileGame().file(path).async("string");
+      let mime = getMimeType(path);
+      let fixedBlob = new Blob([originalBlob], { type: mime });
+      modules[path] = URL.createObjectURL(fixedBlob);
     }
     const module = await import(modules[path]);
     return module;
   }
+
   root.requireModule = requireModule;
 })((typeof window !== 'undefined') ? window : global);
