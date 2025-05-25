478e7c06baaf2a50cbfa959c27f6deb4607c6cc1
Trollhunters501PC <cpcreador@gmail.com>
2025-05-25 17:25:24 -0600

    Arreglar error de JSzip en Async

--- Games/LoadUrlCC.js
+++ Games/LoadUrlCC.js
@@ -1,6 +1,6 @@
 class LoadUrlCC {
     static async fileToUrl(path, mimetype){
-        let subBlob = await GameProps.getFileGame().file(path).Async("blob");
+        let subBlob = await GameProps.getFileGame().file(path).async("blob");
         return URL.createObjectURL(new Blob([subBlob], { type: mimetype || "image/png" }));
     }
     static async setUrlToImg(img, path){
